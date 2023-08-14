a37eb3c4e433f8fa3a793e22b3a9db8b45c05558
pedro-donoso <pedro.donoso.r@usach.cl>
2023-08-14 18:23:28 -0400

    paginacion funcionando

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,8 +3,8 @@ class TweetsController < ApplicationController
 
   # GET /tweets or /tweets.json
   def index
-    # @tweets = Tweet.all
-    @pagy, @tweets = pagy(Tweet.all)
+    @tweets = Tweet.all
+    @pagy, @tweets = pagy(@tweets)
   end
 
   # GET /tweets/1 or /tweets/1.json
