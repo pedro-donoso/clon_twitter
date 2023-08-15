c47c78faf9c6310fd329b752578af769f96df44a
pedro-donoso <pedro.donoso.r@usach.cl>
2023-08-15 10:31:23 -0400

    agrego validacion

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,6 @@
 class Tweet < ApplicationRecord
+   validates :description,  presence: true, 
+                            length: { maximum: 140 }
+   validates :userName,     presence: true, 
+                            length: { maximum: 50 }
 end
